de0e63dab81a82d6515d26cd97408249c5cc8201
Zane <lzy.zane@gmail.com>
2019-03-26 08:51:58 +0800

    同步

---
+++ pattern/src/main/java/threadpool/CachedThreadPoolDemo.java
@@ -0,0 +1,35 @@
+package threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author luzy
+ * viva la vida
+ **/
+public class CachedThreadPoolDemo {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 20; i++) {
+            try {
+                es.submit(() -> {
+                    try {
+                        // Thread sleep randomly for 1 to 5 second
+                        int sleepTime = (int) (Math.random() * 5) + 1;
+                        Thread.sleep(sleepTime);
+                        // print
+                        System.out.println("i'm " + Thread.currentThread().getId() + ", i will sleep " + sleepTime);
+                    } catch (Exception e) {
+                        System.out.println("died");
+                    }
+                });
+                Thread.sleep(1L);
+            } catch (Exception e) {
+                // FUCK
+            }
+        }
+        // When it go through, it will wait 60s to release the threads
+    }
+}
--- pattern/src/main/java/threadpool/Demo.java
+++ pattern/src/main/java/threadpool/Demo.java
@@ -1,8 +1,35 @@
 package threadpool;
 
-/**
- * @author luzy
- * viva la vida
- **/
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Demo {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 20; i++) {
+            es.submit(new MyRunnable(i));
+        }
+
+        es.shutdown();
+    }
+
+    public static class MyRunnable implements Runnable {
+        private int i;
+
+        public MyRunnable(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("This is no." + i);
+                Thread.sleep(1L);
+            } catch (Exception e) {
+                // Hi...
+            }
+        }
+    }
 }
