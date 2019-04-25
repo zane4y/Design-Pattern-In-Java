a02c8c8ea0bd26a2422182a66fb2b462fcd787ee
luzy <luzy@by-health.com>
2019-04-25 14:10:06 +0800

    两个集合类线程不安全的例子

---
+++ pattern/src/main/java/concurrent/ArrayListErrDemo.java
@@ -0,0 +1,38 @@
+package concurrent;
+
+import java.util.ArrayList;
+
+/**
+ * ArrayList 线程不安全的例子
+ *
+ * @author luzy
+ * viva la vida
+ **/
+public class ArrayListErrDemo {
+
+    private static ArrayList<Integer> l = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Runnable r = () -> {
+            int i = 30000;
+            do {
+                l.add(i);
+                i--;
+            } while (i > 0);
+        };
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        Thread t3 = new Thread(r);
+        Thread t4 = new Thread(r);
+        Thread t5 = new Thread(r);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+        System.out.println("End");
+    }
+}
---
+++ pattern/src/main/java/concurrent/HashMapErrDemo.java
@@ -0,0 +1,49 @@
+package concurrent;
+
+import java.util.HashMap;
+
+/**
+ * HashMap线程不安全的例子
+ * @author luzy
+ * viva la vida
+ **/
+public class HashMapErrDemo {
+
+    public static class Key {
+        int val;
+
+        Key(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public int hashCode() {
+            return val % 9;
+        }
+    }
+
+    private static HashMap<Key, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            int i = 50000;
+            do {
+                map.put(new Key(i), i);
+                i--;
+            } while (i > 0);
+        };
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        Thread t3 = new Thread(r);
+        Thread t4 = new Thread(r);
+        Thread t5 = new Thread(r);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+        System.out.println("End");
+    }
+}
